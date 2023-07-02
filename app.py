0a65a5fc8f57757276683dc3bbc220afb75cb635
elizamaharani <elizamaharani@gmail.com>
2023-07-02 21:21:34 +0700

    hello

---
+++ app.py
@@ -0,0 +1,19 @@
+# Importing Flask
+from flask import Flask, jsonify
+
+# Creating an instance of the Flask class
+app = Flask(__name__)
+
+# Creating a new endpoint with this instance as a function decorator
+
+
+@app.route('/')
+def hello_world():
+    return jsonify({
+        "message": "Hello World."
+    })
+
+
+# Starting the Server
+if __name__ == '__main__':
+    app.run()
