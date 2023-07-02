182549686e7801632f4d74f3e113d9a0857fbcc5
elizamaharani <elizamaharani@gmail.com>
2023-07-02 23:55:51 +0700

    add

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({
-        "message": "Hello World."
-    })
-
+    return "HELLO THIS APP"
 
 # Starting the Server
 if __name__ == '__main__':
