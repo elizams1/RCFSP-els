caa2228426737a17f9dba883b76326b2646fcfd0
elizamaharani <elizamaharani@gmail.com>
2023-07-03 10:49:14 +0700

    aw

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,23 +1,25 @@
 # Importing Flask
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import pandas as pd
 # Creating an instance of the Flask class
 app = Flask(__name__)
 
 # Creating a new endpoint with this instance as a function decorator
 df_product = pd.read_csv(
-    'DataProdukVers5.csv')
+    './DataProdukVers5.csv')
 
 df_html = df_product.values
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hello_world():
     return render_template('first-page.html')
 
-@app.route('/second')
+
+@app.route('/second', methods=['GET', 'POST'])
 def sec_page():
     return render_template('second-page.html', data=df_html)
 
+
 # Starting the Server
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
