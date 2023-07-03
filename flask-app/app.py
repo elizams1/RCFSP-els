0afc01a98243f46e473117bc6d53e751fccea66f
elizamaharani <elizamaharani@gmail.com>
2023-07-03 10:07:58 +0700

    runo

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,11 +5,18 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 # Creating a new endpoint with this instance as a function decorator
+df_product = pd.read_csv(
+    '/static/data/DataProdukVers5.csv')
 
+df_html = df_product.values
 
 @app.route('/')
 def hello_world():
     return render_template('first-page.html')
+
+@app.route('/second')
+def sec_page():
+    return render_template('second-page.html', data=df_html)
 
 # Starting the Server
 if __name__ == '__main__':
